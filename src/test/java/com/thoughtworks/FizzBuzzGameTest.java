384ca50b5fcdff39e37e6cfad62fa2f8dec589a2
shuhan <2472520650@qq.com>
2018-05-27 10:43:53 +0800

    init

---
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -0,0 +1,34 @@
+package com.thoughtworks;
+
+import org.junit.Test;
+
+public class FizzBuzzGameTest {
+    @Test
+    public void should_return_Fizz_when_have_3() {
+
+    }
+    @Test
+    public void should_return_Buzz_when_have_5() {
+gi
+    }
+    @Test
+    public void should_return_Whizz_when_have_7(){
+
+    }
+    @Test
+    public void should_return_FizzBuzz_when_have_3and5(){
+
+    }
+    @Test
+    public void should_return_FizzWhizz_when_have_3and7(){
+
+    }
+    @Test
+    public void should_return_BuzzWhizz_when_have_5and7(){
+
+    }
+    @Test
+    public void should_return_FizzBuzzWhizz_when_have_3and5and7(){
+
+    }
+}
